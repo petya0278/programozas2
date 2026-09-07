60079768fa3c5360408cd0dfcd15a6b924807aaf
Petya <oravecz.peter@blathy.info>
2026-09-07 14:35:02 +0200

    oras vege

--- OP2/Program.cs
+++ OP2/Program.cs
@@ -7,7 +7,23 @@
             Console.Title = "Üdvözlő";
             Console.Write("Irj be egy nevet");
             string name=Console.ReadLine();
-            Console.WriteLine($"Szia {name}"); 
+            Console.WriteLine($"Szia {name}");
+
+
+            Console.WriteLine("Irj be ket szamot!");
+
+            Console.Write("Egyik szám: ");
+            string num1=Console.ReadLine();
+
+            Console.Write("Masodik szám: ");
+            string num2 = Console.ReadLine();
+
+            int szamol=int.Parse(num1)+int.Parse(num2);
+            Console.WriteLine($"Eredmény: {szamol}");
+
+
+
+            
         }
     }
 }
