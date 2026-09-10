c2b4fe887b60f4438d64cce4cb37f36959582adb
Rendszergazda <rendszergazda@blathy.info>
2026-09-10 12:20:07 +0200

    ora2

--- OP2/Program.cs
+++ OP2/Program.cs
@@ -1,29 +1,54 @@
-﻿namespace OP2
+﻿using System.ComponentModel.Design;
+
+namespace OP2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Title = "Üdvözlő";
-            Console.Write("Irj be egy nevet");
-            string name=Console.ReadLine();
-            Console.WriteLine($"Szia {name}");
+           
 
 
             Console.WriteLine("Irj be ket szamot!");
 
             Console.Write("Egyik szám: ");
-            string num1=Console.ReadLine();
+            decimal num1 = decimal.Parse(Console.ReadLine());
 
             Console.Write("Masodik szám: ");
-            string num2 = Console.ReadLine();
+            decimal num2 =decimal.Parse(Console.ReadLine());
 
-            int szamol=int.Parse(num1)+int.Parse(num2);
-            Console.WriteLine($"Eredmény: {szamol}");
+            Console.Write("Művelet (+,-,*,/):");
+            string muvelet=Console.ReadLine();
 
+           decimal szam = 0;
 
+            if (muvelet == "+")
+            {
+                szam = num1 + num2;
+            }
+            else if (muvelet == "-") 
+            {
+                szam = num1 - num2;
+            }
+            else if(muvelet == "*")
+            {
+                szam = num1 * num2;
+            }
+            else if (muvelet == "/")
+            {
+                if (num2==0)
+                    Console.WriteLine("Nullával nem osztunk");
+                    
+                szam = num1 / num2;
+                return;
+            }
+            else
+                    {
+                Console.WriteLine("Hibás muvelet");
+                return;
+            }
 
-            
+            Console.WriteLine($"Eredmény {szam}");
         }
     }
 }
