7672d80cab6a97710f8252e3f0962e1fa058fa30
Rendszergazda <rendszergazda@blathy.info>
2026-09-14 14:28:42 +0200

    kk

--- OP2/Program.cs
+++ OP2/Program.cs
@@ -1,54 +1,44 @@
 ﻿using System.ComponentModel.Design;
 
-namespace OP2
+
+
+Console.WriteLine("Irj be ket szamot!");
+
+Console.Write("Egyik szám: ");
+decimal num1 = decimal.Parse(Console.ReadLine());
+
+Console.Write("Masodik szám: ");
+decimal num2 = decimal.Parse(Console.ReadLine());
+
+Console.Write("Művelet (+,-,*,/):");
+string muvelet = Console.ReadLine();
+
+decimal szam = 0;
+
+if (muvelet == "+")
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-           
-
-
-            Console.WriteLine("Irj be ket szamot!");
-
-            Console.Write("Egyik szám: ");
-            decimal num1 = decimal.Parse(Console.ReadLine());
-
-            Console.Write("Masodik szám: ");
-            decimal num2 =decimal.Parse(Console.ReadLine());
-
-            Console.Write("Művelet (+,-,*,/):");
-            string muvelet=Console.ReadLine();
-
-           decimal szam = 0;
-
-            if (muvelet == "+")
-            {
-                szam = num1 + num2;
-            }
-            else if (muvelet == "-") 
-            {
-                szam = num1 - num2;
-            }
-            else if(muvelet == "*")
-            {
-                szam = num1 * num2;
-            }
-            else if (muvelet == "/")
-            {
-                if (num2==0)
-                    Console.WriteLine("Nullával nem osztunk");
-                    
-                szam = num1 / num2;
-                return;
-            }
-            else
-                    {
-                Console.WriteLine("Hibás muvelet");
-                return;
-            }
-
-            Console.WriteLine($"Eredmény {szam}");
-        }
-    }
+    szam = num1 + num2;
 }
+else if (muvelet == "-")
+{
+    szam = num1 - num2;
+}
+else if (muvelet == "*")
+{
+    szam = num1 * num2;
+}
+else if (muvelet == "/")
+{
+    if (num2 == 0)
+        Console.WriteLine("Nullával nem osztunk");
+
+    szam = num1 % num2;
+    return;
+}
+else
+{
+    Console.WriteLine("Hibás muvelet");
+    return;
+}
+
+Console.WriteLine($"Eredmény {szam}");
---
+++ ProgramozasiTetelek/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ProgramozasiTetelek
+{
+    
+    internal class Program
+    {
+        public static int Algoritmus(int[] Tomb, int tomblenght)
+        {
+            int i = 0;
+            
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
