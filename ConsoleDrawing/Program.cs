94e060caf7120bd10dd9eff6b400de971ce477e3
Petya <oravecz.peter@blathy.info>
2026-09-21 15:25:28 +0200

    itt tartok

---
+++ ConsoleDrawing/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleDrawing
+{
+    internal class Program
+    //█░▒▓   ╔╗═║╝╚
+
+    {
+        static void DrawBorder()
+        {
+            //1.
+
+            Console.Write("╔");
+
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("═");
+
+            }
+           
+            
+            static void Main(string[] args)
+            {
+                DrawBorder();
+                Console.CursorVisible = true;
+                int width= Console.WindowWidth;
+                int height= Console.WindowHeight;
+
+                for (int i = 0; i < width; i++) { 
+                Console.SetCursorPosition(i, 0);
+                    Console.Write('═');
+                }
+            }
+        }
+    }
+}
