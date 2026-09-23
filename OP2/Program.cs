ebbc5a0049e501def60aca1912946bf2567b5a20
petil <petil@DESKTOP-L35U5O4>
2026-09-23 20:33:36 +0200

    kesz szg

--- OP2/Program.cs
+++ OP2/Program.cs
@@ -1,40 +1,64 @@
-﻿using System.ComponentModel.Design;
-//eldontes tetele
+﻿using System;
 
-
-Console.WriteLine("Irj be ket szamot!");
-
-Console.Write("Egyik szám: ");
-decimal num1 = decimal.Parse(Console.ReadLine());
-
-Console.Write("Masodik szám: ");
-decimal num2 = decimal.Parse(Console.ReadLine());
-
-Console.Write("Művelet (+,-,*,/):");
-string muvelet = Console.ReadLine();
-
-decimal szam = 0;
-
-if (muvelet == "+")
+class Program
 {
-    szam = num1 + num2;
-}
-else if (muvelet == "-")
-{
-    szam = num1 - num2;
-}
-else if (muvelet == "*")
-{
-    szam = num1 * num2;
-}
-else if (muvelet == "/" && num2 != 0)
-{
-    szam = num1 % num2;
-}
-else
-{
-    Console.WriteLine("Hibás muvelet");
-    
+    static void Main()
+    {
+        Console.WriteLine("Számológép");
+
+        Console.Write("Írd be az első számot: ");
+        double szam1;
+
+        while (!double.TryParse(Console.ReadLine(), out szam1))
+        {
+            Console.Write("Hibás adat! Csak számot írj be: ");
+        }
+
+        Console.Write("Írd be a műveletet (+, -, *, /): ");
+        string muvelet = Console.ReadLine();
+
+        while (muvelet != "+" && muvelet != "-" &&
+               muvelet != "*" && muvelet != "/")
+        {
+            Console.Write("Hibás művelet! Írd be: +, -, * vagy /: ");
+            muvelet = Console.ReadLine();
+        }
+
+        Console.Write("Írd be a második számot: ");
+        double szam2;
+
+        while (!double.TryParse(Console.ReadLine(), out szam2))
+        {
+            Console.Write("Hibás adat! Csak számot írj be: ");
+        }
+
+        double eredmeny = 0;
+
+        
+        if (muvelet == "+")
+        {
+            eredmeny = szam1 + szam2;
+        }
+        else if (muvelet == "-")
+        {
+            eredmeny = szam1 - szam2;
+        }
+        else if (muvelet == "*")
+        {
+            eredmeny = szam1 * szam2;
+        }
+        else if (muvelet == "/")
+        {
+            if (szam2 == 0)
+            {
+                Console.WriteLine("Hiba! Nullával nem lehet osztani.");
+                return;
+            }
+
+            eredmeny = szam1 / szam2;
+        }
+
+        Console.WriteLine("Eredmény: " + eredmeny);
+    }
 }
 
-Console.WriteLine($"Eredmény {szam}");
